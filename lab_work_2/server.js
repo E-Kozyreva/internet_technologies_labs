523a1d5602e360c3b3adbf884966de2d69060f98
E-Kozyreva <kozyreva.k1@mail.ru>
2023-12-15 23:35:09 +0300

    v.1.1.0

--- lab_work_2/server.js
+++ lab_work_2/server.js
@@ -1,51 +1,122 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const session = require('express-session');
+const flash = require('connect-flash');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const MongoClient = require('mongodb').MongoClient;
+
+// Переименованные переменные
+let booksCollection;
+let userCollection;
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('pages'));
+app.set('view engine', 'ejs');
 
-mongoose.connect('mongodb://localhost:27017/login_registration_db', {
+// Используем сессии и middleware для flash-сообщений
+app.use(session({
+  secret: 'ваш_секретный_ключ',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(flash());
+
+MongoClient.connect('mongodb://localhost:27017/', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-});
-
-const User = mongoose.model('User', userSchema);
-
-app.post('/register', async (req, res) => {
-  const { username, password } = req.body;
-
-  try {
-    const user = new User({ username, password });
-    await user.save();
-    res.status(201).send('User registered successfully.');
-  } catch (error) {
-    res.status(500).send('Error registering user.');
+  useUnifiedTopology: true
+}, (err, database) => {
+  if (err) {
+    return console.log(err);
   }
+
+  // Инициализируем коллекции
+  booksCollection = database.db('database').collection('books');
+  userCollection = database.db('database').collection('user', (err, res) => {
+    res.estimatedDocumentCount((err, count) => {
+      if (count == 0) {
+        database.db('database').createCollection('user');
+      }
+    });
+  });
+
+  // Запускаем сервер
+  app.listen(3000, function () {
+    console.log(`Сервер запущен по адресу http://localhost:3000`);
+  });
 });
 
-app.post('/login', async (req, res) => {
-  const { username, password } = req.body;
+// Обрабатываем GET-запрос для главной страницы
+app.get('/', (req, res) => {
+  booksCollection.find({}).toArray((err, items) => {
+    res.render('F:/university/internet_technologies_labs/lab_work_2/pages/main.ejs', {
+      books: items,
+      messages: req.flash('error') // Передаем flash-сообщения в шаблон
+    });
+  });
+});
 
-  try {
-    const user = await User.findOne({ username, password });
-    if (user) {
-      res.status(200).send('Login successful.');
+// Обрабатываем POST-запрос для главной страницы (взятие книги в аренду)
+app.post('/', (req, res) => {
+  userCollection.estimatedDocumentCount((err, count) => {
+    if (count != 0) {
+      req.flash('error', 'Пожалуйста, верните книгу перед тем, как взять новую :)');
+      res.redirect('/');
     } else {
-      res.status(401).send('Invalid credentials.');
+      booksCollection.findOneAndUpdate({ 'id': req.body.book_id }, { $inc: { 'count': -1 } }, (err, book) => {
+        if (book.value.count == 0) {
+          booksCollection.findOneAndUpdate({ 'id': book.value.id }, { $set: { 'count': 0 } });
+          res.redirect('/');
+        } else {
+          book.value.count = 1;
+          const date = new Date();
+          date.setFullYear(date.getFullYear(), date.getMonth() + 1, date.getDate() + 20);
+          const formattedDate = `${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`;
+          userCollection.insertOne({ ...book.value, 'date': formattedDate });
+          res.redirect('/');
+        }
+      });
     }
-  } catch (error) {
-    res.status(500).send('Error logging in.');
-  }
+  });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// Обрабатываем GET-запрос для страницы пользователя
+app.get('/user', (req, res) => {
+  userCollection.find({}).toArray((err, items) => {
+    res.render('F:/university/internet_technologies_labs/lab_work_2/pages/user.ejs', {
+      books: items
+    });
+  });
+});
+
+// Обрабатываем POST-запрос для страницы пользователя (возвращение книги)
+app.post('/user', (req, res) => {
+  booksCollection.findOneAndUpdate({ 'id': req.body.book_id }, { $inc: { 'count': 1 } });
+  userCollection.deleteOne({ 'id': req.body.book_id });
+  res.redirect('/user');
+});
+
+// Обрабатываем GET-запрос для страницы добавления книги
+app.get('/addBook', (req, res) => {
+  res.render('F:/university/internet_technologies_labs/lab_work_2/pages/add_book.ejs');
+});
+
+// Обрабатываем POST-запрос для страницы добавления книги
+app.post('/addBook', (req, res) => {
+  const book = {
+    'id': req.body.id,
+    'authors': req.body.authors,
+    'editioncode': req.body.editioncode,
+    'edition': req.body.edition,
+    'name': req.body.name,
+    count: Number(req.body.count)
+  };
+
+  booksCollection.insertOne(book, err => {
+    if (err) {
+      res.send({ 'error': 'Произошла ошибка' });
+    } else {
+      res.redirect('/');
+    }
+  });
 });
